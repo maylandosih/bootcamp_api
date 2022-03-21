7cfeb7e6a91f0199b5d685f28b47bb80ecdfce99
maylando sihombing <maylandosihombing@MacBook-Pro.local>
2022-03-22 00:14:38 +0700

    initial commit

--- routers/index.js
+++ routers/index.js
@@ -2,6 +2,7 @@ const usersRouter = require('./usersRouter');
 const productsRouter = require('./productsRouter')
 const transactionsRouter = require('./transactionsRouter')
 
+
 module.exports = {
     usersRouter,
     productsRouter,
